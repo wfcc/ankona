1d30295561f2c1cf184367e0b63dea41da1a92b3
Iļja Ketris <ike@vo.id.lv>
2011-03-03 14:08:26 +0200

    login widget redone

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,39 +1,10 @@
 $(function() {
   $('.button').button()
+
+  $('#lg_menu').click(function() {
+    $('#lg_submenu').toggle()
+    })
+  
   })
 // -------------------------------------------------------------------
-function dynamicAddRemove(parent, parents, child, children) {
-  alert('a.' + child + '-add');
-  $('a.' + child + '-add').live('click', function() {
-    var parentId = $(this).closest('.' + children).attr('id').replace(parent + '-', '');
-    indexData = ''
-    // check to see if there is a parent div; this will be the case if we're doing a
-    // grandchild (or deeper) insert
-    if ($(this).closest('div.' + parent).size() > 0) {
-      var matcher = new RegExp (parents + '_attributes\\]\\[(\\d*)\\]');
-      var parentIndex = $(this).closest('div.' + parent).find(':input').attr('name').match(matcher)[1];
-      indexData = 'index=' + parentIndex;
-      }
-    $.get('/' + parents + '/' + parentId + '/' + children + '/new', indexData, function(data) {
-      $(data).hide().appendTo($('div.' + children + '#' + parent + '-' + parentId + ' div#new-' + children)).fadeIn('slow');
-    });
-    return false;
-    });
-  $('a.' + child + '-remove').live('click', function() {
-    var childId = $(this).closest('.' + child).attr('id');
-    var parentId = $(this).closest('.' + children).attr('id').replace(parent + '-', '');
-    $.post('/' + parents + '/' + parentId + '/' + children + '/' + childId, { _method: 'delete' }, function(data, textStatus) {
-      var response = JSON.parse(data);
-      if (response.success) {
-        $('div.' + child + '#' + childId).fadeOut('slow', function() {
-          $(this).remove();
-          });
-        }
-      else {
-        alert('The ' + child + ' could not be removed because ' + response.msg);
-        }
-      });
-      return false;
-    });
-  }
 
