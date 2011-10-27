2be1fa93804662dc5a7bff5daaa278a0cd2575f0
Iļja Ketris <ike@vo.id.lv>
2011-10-27 12:21:36 +0300

    assets compiled II

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module DiaX
     config.assets.precompile += [ 'diagram_edit.js', 'users.js' ]
       
     # Version of your assets, change this if you want to expire all your assets  
-    config.assets.version = '1.02'
+    config.assets.version = '1.03'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
