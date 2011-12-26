67bee93b2f85e1a07a0e52ad43806f3f454afd1b
Iļja Ketris <ike@vo.id.lv>
2011-12-26 17:39:30 +0200

    assets!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ DiaX::Application.configure do
        
   # Don't fallback to assets pipeline  
   config.assets.compile = false  
-  config.assets.precompile << '*.js'
+  config.assets.precompile += ['*.js', /css/]
        
   # Generate digests for assets URLs  
   config.assets.digest = true
